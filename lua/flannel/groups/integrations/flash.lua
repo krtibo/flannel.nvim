5e3881b3d6c53626992e0a6ec81c676fdfdb16b2
Tibor Krizsák <t.krizsak@topdesk.com>
2024-05-25 20:23:47 +0200

    adjust flash integration highlight colors

--- lua/flannel/groups/integrations/flash.lua
+++ lua/flannel/groups/integrations/flash.lua
@@ -1,12 +1,11 @@
 local M = {}
 
 function M.get()
-	local bg = O.transparent_background and C.none or C.base
 	return {
 		FlashBackdrop = { fg = C.overlay0 },
-		FlashLabel = { fg = C.green, bg = bg, style = { "bold" } },
-		FlashMatch = { fg = C.lavender, bg = bg },
-		FlashCurrent = { fg = C.peach, bg = bg },
+		FlashLabel = { fg = C.base, bg = C.green, style = { "bold" } },
+		FlashMatch = { fg = C.base, bg = C.red },
+		FlashCurrent = { fg = C.base, bg = C.blue },
 		FlashPrompt = { link = "NormalFloat" },
 	}
 end
